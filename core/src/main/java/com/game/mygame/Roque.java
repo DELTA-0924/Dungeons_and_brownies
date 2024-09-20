0cb2ff8e2818d1f6bed9330bdab3b6166812ad9e
Timur <scorpik1992@gmail.com>
2024-09-20 13:27:12 +0600

    make mainmenu and add two buttons make for him base style

--- core/src/main/java/com/game/mygame/Roque.java
+++ core/src/main/java/com/game/mygame/Roque.java
@@ -1,8 +1,12 @@
 package com.game.mygame;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class Roque extends Game {
     public SpriteBatch batch;
